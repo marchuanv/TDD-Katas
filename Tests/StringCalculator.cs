46b3415e217be6c3d304060805221ed297d535f1
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-03-31 03:17:50 +0200

    updated project

---
+++ Tests/StringCalculator.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TDD_Katas.Tests
+{
+    [TestFixture]
+    public class StringCalculator
+    {
+        [Test]
+        public void Test() {
+
+            throw new Exception("ERROR 3");
+        }
+    }
+}
